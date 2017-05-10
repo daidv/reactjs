02ceb05ee59d21a2dd6735dd6965e5b6d8ba729c
daidv <lebaduy94@gmail.com>
2017-05-10 04:30:50 -0700

    chieu 10/5/17

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var app = express();
 app.use(express.static("public"));
 app.set("view engine","ejs");
@@ -7,6 +9,30 @@ app.listen(1337,function(){
   console.log("connect to server success with port 1337 !!! ");
 });
 
+var mang = ["Android","IOS","PHP","React"];
+
 app.get("/",function(req, res){
   res.render("trangchu");
+});
+
+app.post('/getNotes', function(req, res){
+  res.send(mang);
+})
+
+app.post('/add',urlencodedParser,function(req, res){
+  var newNote = req.body.note;
+  mang.push(newNote);
+  res.send(mang);
+});
+
+app.post('/delete',urlencodedParser, function(req,res){
+  var id = req.body.idXoa;
+  mang.splice(id,1);
+  res.send(mang);
+});
+
+app.post('/update',urlencodedParser, function(req,res){
+  var id = req.body.idSua;
+  mang[id] = req.body.noiDung;
+  res.send(mang);
 });
--- public/xuly.js
+++ public/xuly.js
@@ -1,52 +1,104 @@
 
-var Note = React.createClass({
-  render:function(){
-    return <div className="div-note">
-              { this.props.children }
-            </div>
-  }
-});
+var list;
 function addDiv(){
   ReactDOM.render(
     <InputDiv />, document.getElementById('div-add')
   );
 }
+
 var List = React.createClass({
   getInitialState(){
+    list = this;
     return{
-      mang: ["hi","Hello","Me"]
+      mang: []
     }
   },
-  render: function(){
+  render(){
     return(
       <div className="div-list">
         <div id="div-add"></div>
-      <button onClick={addDiv}>Them</button>
-        {
-          this.state.mang.map(function(note,index){
-            return <Note key={index}> {note} </Note>
-          })
-        }
-      </div>
+        <button onClick={addDiv}>Them</button>
+          {
+            this.state.mang.map(function(note, index){
+              return <Note key={index} id={index}>{note}</Note>
+            })
+          }
+    </div>
     );
+  },
+  componentDidMount(){
+    var that = this;
+    $.post("/getNotes",function(data){
+      that.setState({mang: data});
+    });
   }
 });
-var InputDiv = React.createClass({
-  send(){
 
+var Note = React.createClass({
+  save(){
+    var note = this;
+      $.post("/update",{idSua: this.props.id, noiDung: this.refs.txt.value },function(data){
+          list.setState({mang:data});
+          note.setState({onEdit: false})
+      });
+  },
+  cancel(){
+    this.setState({onEdit: false});
+  },
+  onEdit(){
+    this.setState({onEdit: true});
+  },
+  getInitialState(){
+    return{
+      onEdit: false
+    };
+  },
+  delete(){
+    $.post('/delete',{idXoa: this.props.id},function(data){
+        list.setState({mang: data});
+    });
   },
   render: function(){
+    if(this.state.onEdit == true){
+      return(
+          <div className="div-note">
+            <input ref="txt" defaultValue={this.props.children} />
+            <button onClick={this.save}>Luu</button>
+            <button onClick={this.cancel}>Huu</button>
+          </div>
+      );
+    }else{
+    return(
+        <div className="div-note">
+          <p>{this.props.children}</p>
+          <button onClick={this.delete}>delete</button>
+          <button onClick={this.onEdit}>edit</button>
+        </div>
+    );
+  }
+  }
+});
+
+var InputDiv = React.createClass({
+  send(){
+    $.post('/add',{note: this.refs.txt.value},function(data){
+      list.setState({mang: data})
+    });
+    ReactDOM.unmountComponentAtNode(document.getElementById('div-add'))
+  },
+  render(){
     return(
       <div>
-        <input type="text" ref="txt" placeHolder="Enter your note !"/>
+        <input type="text" ref="txt" placeholder="enter your note ! "/>
         <button onClick={this.send}>Gui</button>
       </div>
     );
   }
 });
+
 ReactDOM.render(
   <div>
     <List />
-  </div>
-  ,document.getElementById('root')
+  </div>,
+  document.getElementById('root')
 );
