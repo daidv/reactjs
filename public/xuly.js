120edd63bffcf9d28a7d817ee81c701e185a5cf4
daidv <lebaduy94@gmail.com>
2017-05-09 21:41:47 -0700

    10/5 cty

--- public/xuly.js
+++ public/xuly.js
@@ -1,22 +1,52 @@
 
-
-var Com = React.createClass({
-  add(){
-    this.setState({num: parseInt(this.state.num) + 1});
-  },
+var Note = React.createClass({
+  render:function(){
+    return <div className="div-note">
+              { this.props.children }
+            </div>
+  }
+});
+function addDiv(){
+  ReactDOM.render(
+    <InputDiv />, document.getElementById('div-add')
+  );
+}
+var List = React.createClass({
   getInitialState(){
     return{
-      num:0
+      mang: ["hi","Hello","Me"]
     }
   },
   render: function(){
     return(
-      <button onClick={this.add}>Hello {this.state.num} </button>
+      <div className="div-list">
+        <div id="div-add"></div>
+      <button onClick={addDiv}>Them</button>
+        {
+          this.state.mang.map(function(note,index){
+            return <Note key={index}> {note} </Note>
+          })
+        }
+      </div>
     );
   }
 });
+var InputDiv = React.createClass({
+  send(){
 
+  },
+  render: function(){
+    return(
+      <div>
+        <input type="text" ref="txt" placeHolder="Enter your note !"/>
+        <button onClick={this.send}>Gui</button>
+      </div>
+    );
+  }
+});
 ReactDOM.render(
-  <Com />,
-  document.getElementById('root')
+  <div>
+    <List />
+  </div>
+  ,document.getElementById('root')
 );
